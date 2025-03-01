25d6857db60a1b1451705be054f0f35713bef0d6
Egorixa <92587283+Egorixa@users.noreply.github.com>
2025-03-01 21:27:40 +0300

    initial commit

---
+++ TaskForNis/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TaskForNis;
+
+public class Program
+{
+    public static void Main()
+    {
+        do
+        {
+            Console.Write("Введи число a: ");
+            string a = Console.ReadLine();
+            if (decimal.TryParse(a, out decimal n))
+            {
+                Console.Write("Введи число b: ");
+                string b = Console.ReadLine();
+                if (decimal.TryParse(b, out decimal n2))
+                {
+                    Console.WriteLine($"Сумма чисел: {n + n2:f2}");
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не число(");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число(");
+            }
+            
+            Console.WriteLine("Хотите выйти? y/smth_else");
+            string? exit  = Console.ReadLine();
+            if (exit == null ||  exit.ToLower() == "y")
+            {
+                break;
+            }
+        } while (true);
+        
+    }
+}
